d3b05f19f62b2f39b7be99557ec2c6935c491bc8
yerim jeon <wjsdpfla@gmail.com>
2021-10-06 10:17:52 +0900

    Feat: add login component and logic

---
+++ src/pages/Login.js
@@ -0,0 +1,65 @@
+import React from "react";
+import firebase from "../config/firebase";
+import { useMutation } from "react-query";
+import { useHistory } from "react-router-dom";
+import styled from "styled-components";
+
+import { postLogin } from "../api";
+
+const Login = ({ setUser }) => {
+  const history = useHistory();
+
+  const { mutate } = useMutation(postLogin, {
+    onSuccess: ({ data }) => {
+      const { user } = data;
+
+      setUser(user);
+
+      if (!user.couple && !user.partner_id) {
+        history.push("/register");
+        return;
+      }
+
+      if (!user.couple && user.partner_id) {
+        history.push("/queue");
+        return;
+      }
+
+      if (user.couple && !user.is_matched) {
+        history.push("/queue");
+        return;
+      }
+
+      history.push("/chat");
+    },
+  });
+
+  const loginWithGoogle = async () => {
+    try {
+      const provider = new firebase.auth.GoogleAuthProvider();
+      const { additionalUserInfo } = await firebase.auth().signInWithPopup(provider);
+      const { email, name, picture } = additionalUserInfo.profile;
+
+      const userData = {
+        name,
+        email,
+        picture,
+      };
+
+      mutate(userData);
+    } catch (error) {
+
+    }
+  };
+
+  return (
+    <Wrapper>
+      <button type="button" onClick={loginWithGoogle}>Google Login</button>
+    </Wrapper>
+  );
+};
+
+const Wrapper = styled.div`
+`;
+
+export default Login;
